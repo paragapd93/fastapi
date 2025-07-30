12c9dcc0d4175b18925f67a2589238510b2e291f
Parag Mitra <75339409+paragapd93@users.noreply.github.com>
2025-07-30 22:04:34 +0530

    Add files via upload

--- Patient_Profile.py
+++ Patient_Profile.py
@@ -145,7 +145,18 @@ def update_patient(patient_id: str, patient_update: PatientUpdate): #patient_upd
 
     return JSONResponse(status_code=200, content="Patient updated successfully!")
 
+@app.delete("/delete/{patient_id}")
+def delete_patient(patient_id: str):
+    data = load_data()
 
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found!")
+
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content="Patient deleted!")
 
 
 
